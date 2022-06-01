86eb3fa653a0dd70434f752ea93e1c5abcfcf3c8
oswin <ozie03@gmail.com>
2022-06-01 19:48:18 +0200

    Initial project setup

---
+++ src/main/kotlin/zw/co/simba/stands/standsmanagement/StandsManagementApplication.kt
@@ -0,0 +1,11 @@
+package zw.co.simba.stands.standsmanagement
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class StandsManagementApplication
+
+fun main(args: Array<String>) {
+    runApplication<StandsManagementApplication>(*args)
+}
---
+++ src/test/kotlin/zw/co/simba/stands/standsmanagement/StandsManagementApplicationTests.kt
@@ -0,0 +1,13 @@
+package zw.co.simba.stands.standsmanagement
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class StandsManagementApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
